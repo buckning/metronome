bd2d441fc686d2725b3a8b8ef2a5f4e1040c1537
amcglynn <amcglynn@cisco.com>
2018-01-05 17:35:53 +0000

    Added tests for the clamp function in CLI metronome

--- test/java/com/amcglynn/metronome/CommandLineMetronomeTest.kt
+++ test/java/com/amcglynn/metronome/CommandLineMetronomeTest.kt
@@ -1,8 +1,29 @@
 package com.amcglynn.metronome
 
+import metronome.CommandLineMetronome
+import org.junit.Test
+import kotlin.test.assertEquals
+
 /**
  * Created by amcglynn on 05/01/2018.
  */
 class CommandLineMetronomeTest {
 
+    @Test
+    fun testClampReturnsMinNumberWhenNumberBelowMinimumIsPassedIn() {
+        val cliMetronome = CommandLineMetronome()
+        assertEquals(25f, cliMetronome.clamp(10f, 25f, 100f))
+    }
+
+    @Test
+    fun testClampReturnsMaxNumberWhenNumberAboveTheMaximumIsPassedIn() {
+        val cliMetronome = CommandLineMetronome()
+        assertEquals(250f, cliMetronome.clamp(1000f, 25f, 250f))
+    }
+
+    @Test
+    fun testClampReturnsTheNumberWhenNumberIsWithinTheLimits() {
+        val cliMetronome = CommandLineMetronome()
+        assertEquals(50f, cliMetronome.clamp(50f, 25f, 250f))
+    }
 }
